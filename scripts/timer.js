da0747aedbd39f9bb63e06fab60db7ce10c7687f
pedroacamargo <pedroaugennes@gmail.com>
2022-12-18 01:03:33 +0000

    start()

---
+++ scripts/timer.js
@@ -0,0 +1,47 @@
+// variables
+let breakTitle = document.getElementById("break")
+let resetBtn = document.getElementById("reset")
+let click = 0
+let workTime = 50
+let breakTime = 10
+
+let seconds = "00"
+
+// when open the website
+window.onload = () => {
+    document.getElementById("minutes").innerHTML = workTime
+    document.getElementById("seconds").innerHTML = seconds
+}
+
+// start timer
+function start() {
+    seconds = 59
+    
+
+    let workMinutes = workTime - 1
+    let breakMinute = breakTime - 1
+
+
+    // countdown
+    let timerFunction = () => {
+        // change the display
+        document.getElementById("minutes").innerHTML = workMinutes
+        document.getElementById("seconds").innerHTML = seconds
+
+
+        seconds--
+        if (seconds == 0) {
+            seconds = 59
+            workMinutes--
+            if (workMinutes == -1) {
+                workMinutes = workTime
+            }
+        }
+
+    }
+    
+    
+    // each second do this
+    setInterval(timerFunction, 1000) // 1000 = 1s
+   
+}
